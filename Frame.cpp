c741ecf525221e9e11e76579856e47f0f4c878c4
igorank <gotogo1995@gmail.com>
2021-08-14 22:14:20 +0300

    Made window non-resizable

--- Frame.cpp
+++ Frame.cpp
@@ -1,6 +1,6 @@
 #include "Frame.h"
 
-Frame::Frame(const wxString& title, int width, int height) : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(width, height))
+Frame::Frame(const wxString& title, int width, int height) : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(width, height), wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX))
 {
 	Centre();
 	SetIcon(wxIcon("data\\icon.ico", wxBITMAP_TYPE_ICO));
