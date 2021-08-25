7aa523b9e85185b8dbb8761bfa0ee63a85db99cb
Igor Anikin <anikinigor165@gmail.com>
2021-08-25 14:17:18 +0300

    Added config file

--- SettingsDialog.cpp
+++ SettingsDialog.cpp
@@ -5,6 +5,9 @@ SettingsDialog::SettingsDialog(const wxString& title) :
 {
 	wxPanel* panel = new wxPanel(this, 1);
 
+	OpenConfFile("data\\config.cfg");
+	ReadConfFile();
+
 	wxBoxSizer* vbox = new wxBoxSizer(wxVERTICAL);
 	wxBoxSizer* hbox = new wxBoxSizer(wxHORIZONTAL);
 
@@ -17,14 +20,14 @@ SettingsDialog::SettingsDialog(const wxString& title) :
 	wxStaticText* PomToLongBreak = new wxStaticText(this, wxID_ANY, wxT("Pomodoros to a long break:"),
 		wxPoint(25, 183));
 
-	wxSlider* SessionTimeSlider = new wxSlider(panel, 100, 25, 5, 55,
+	wxSlider* SessionTimeSlider = new wxSlider(panel, 100, GetSessionTime(), 5, 55,
 		wxPoint(100, 30), wxSize(140, -1), wxSL_LABELS | wxSL_TOP);
-	wxSlider* ShortBreakSlider = new wxSlider(panel, 101, 5, 1, 30,
+	wxSlider* ShortBreakSlider = new wxSlider(panel, 101, GetShortBreakTime(), 1, 30,
 		wxPoint(100, 80), wxSize(140, -1), wxSL_LABELS | wxSL_TOP);
-	wxSlider* LongBreakSlider = new wxSlider(panel, 102, 15, 5, 55,
+	wxSlider* LongBreakSlider = new wxSlider(panel, 102, GetLongBreakTime(), 5, 55,
 		wxPoint(100, 130), wxSize(140, -1), wxSL_LABELS | wxSL_TOP);
-	wxSpinCtrl* PomToLongSpinCtrl = new wxSpinCtrl(panel, wxID_ANY, '4', wxPoint(180, 180), wxDefaultSize,
-		wxSP_ARROW_KEYS, 1, 8, 4);
+	wxSpinCtrl* PomToLongSpinCtrl = new wxSpinCtrl(panel, wxID_ANY, GetPomToLongBreakChar(), wxPoint(180, 180), wxDefaultSize,
+		wxSP_ARROW_KEYS, 1, 8, GetPomToLongBreak());
 
 	wxButton* okButton = new wxButton(this, -1, wxT("Ok"),
 		wxDefaultPosition, wxSize(70, 30));
@@ -39,5 +42,30 @@ SettingsDialog::SettingsDialog(const wxString& title) :
 	Centre();
 	ShowModal();
 
+	CloseConfFile();
 	Destroy();
+}
+
+void SettingsDialog::OpenConfFile(const char* name)
+{
+	file.open(name);
+}
+
+void SettingsDialog::CloseConfFile()
+{
+	file.close();
+}
+
+void SettingsDialog::ReadConfFile()
+{
+	file >> PomodoroSessionTime;
+	file >> ShortBreakTime;
+	file >> LongBreakTime;
+	file >> PomToLongBreak;
+}
+
+char SettingsDialog::GetPomToLongBreakChar()
+{
+	PomToLongBreakChar = '0' + PomToLongBreak;
+	return PomToLongBreakChar;
 }
--- SettingsDialog.h
+++ SettingsDialog.h
@@ -7,12 +7,32 @@
 #endif
 
 #include <wx/spinctrl.h>
+#include <fstream>
 
 
 class SettingsDialog : public wxDialog
 {
+	private:
+		std::ifstream file;
+		char filename[50];
+
+		int PomodoroSessionTime;
+		int ShortBreakTime;
+		int LongBreakTime;
+		int PomToLongBreak;
+		char PomToLongBreakChar;
 	public:
 		SettingsDialog(const wxString& title);
+		void OpenConfFile(const char* name);
+		void CloseConfFile();
+		void ReadConfFile();
+
+		int GetSessionTime() { return PomodoroSessionTime; }
+		int GetShortBreakTime() { return ShortBreakTime; }
+		int GetLongBreakTime() { return LongBreakTime; }
+		int GetPomToLongBreak() { return PomToLongBreak; }
+
+		char GetPomToLongBreakChar();
 };
 
 #endif
